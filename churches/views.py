52e1ece0884dbfe8f52271b3013e42a423cad117
tramcarjobs <38306928+tramcarjobs@users.noreply.github.com>
2023-12-31 19:52:10 +0000

    Initial commit

---
+++ churches/views.py
@@ -0,0 +1,15 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Page
+
+
+def index(request):
+    page = get_object_or_404(Page, pk=1)
+    return render(request, "churches/index.html", {"page": page})
+
+
+def detail(request, page_id):
+    page = get_object_or_404(Page, pk=page_id)
+    return render(request, "churches/detail.html", {"page": page})
