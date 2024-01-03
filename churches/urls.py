adebac83165dafcaaaae6365faee5da118f35d3f
tramcarjobs <38306928+tramcarjobs@users.noreply.github.com>
2024-01-03 02:46:48 +0000

    First pass at adding markdown support

--- churches/urls.py
+++ churches/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:page_id>/", views.detail, name="detail"),
-    path("<str:page_slug>/", views.detail_slug, name="detail_slug"),
+    path("pages/<int:page_id>/", views.detail, name="detail"),
+    path("pages/<str:page_slug>/", views.detail_slug, name="detail_slug"),
 ]
--- churches/views.py
+++ churches/views.py
@@ -1,20 +1,26 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+
+import markdown
 
 from .models import Page
 
 
 def index(request):
     page = get_object_or_404(Page, pk=1)
-    return render(request, "churches/index.html", {"page": page})
+    # TODO: Sanitize markdown
+    content_md = markdown.markdown(page.content)
+    return render(request, "churches/index.html", {"page": page, "content_md": content_md})
 
 
 def detail(request, page_id):
     page = get_object_or_404(Page, pk=page_id)
-    return render(request, "churches/detail.html", {"page": page})
+    # TODO: Sanitize markdown
+    content_md = markdown.markdown(page.content)
+    return render(request, "churches/detail.html", {"page": page, "content_md": content_md})
 
 
 def detail_slug(request, page_slug):
     page = get_object_or_404(Page, slug=page_slug)
-    return render(request, "churches/detail.html", {"page": page})
+    # TODO: Sanitize markdown
+    content_md = markdown.markdown(page.content)
+    return render(request, "churches/detail.html", {"page": page, "content_md": content_md})
