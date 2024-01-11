071cb16f2edc720483ae28aa61516285a2304fc5
tramcarjobs <38306928+tramcarjobs@users.noreply.github.com>
2024-01-11 04:07:13 +0000

    Add page ordering

--- churches/models.py
+++ churches/models.py
@@ -5,8 +5,12 @@ class Page(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     slug = models.SlugField(max_length=50, unique=True)
+    order = models.PositiveSmallIntegerField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
         return self.title
