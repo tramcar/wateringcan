885b9cc1aa7a5bb7e76b72aa0a01e4cda3c5eb42
tramcarjobs <38306928+tramcarjobs@users.noreply.github.com>
2024-01-27 20:33:42 +0000

    Rename app

--- wateringcan/views.py
+++ wateringcan/views.py
@@ -14,21 +14,21 @@ def index(request):
     page = get_object_or_404(Page, order=0)
     # TODO: Sanitize markdown
     content_md = markdown.markdown(page.content)
-    return render(request, "churches/index.html", {"page": page, "content_md": content_md})
+    return render(request, "wateringcan/index.html", {"page": page, "content_md": content_md})
 
 
 def detail(request, page_id):
     page = get_object_or_404(Page, pk=page_id)
     # TODO: Sanitize markdown
     content_md = markdown.markdown(page.content)
-    return render(request, "churches/detail.html", {"page": page, "content_md": content_md})
+    return render(request, "wateringcan/detail.html", {"page": page, "content_md": content_md})
 
 
 def detail_slug(request, page_slug):
     page = get_object_or_404(Page, slug=page_slug)
     # TODO: Sanitize markdown
     content_md = markdown.markdown(page.content)
-    return render(request, "churches/detail.html", {"page": page, "content_md": content_md})
+    return render(request, "wateringcan/detail.html", {"page": page, "content_md": content_md})
 
 
 def contact_us(request):
@@ -46,4 +46,4 @@ def contact_us(request):
     else:
         form = ContactUsForm()
 
-    return render(request, "churches/contact_us.html", {"form": form})
+    return render(request, "wateringcan/contact_us.html", {"form": form})
